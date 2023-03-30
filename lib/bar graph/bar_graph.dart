04a7f4d5161c0f4c7905b717729f93208f4d3ba8
Sommos <Sommosgaming@gmail.com>
2023-03-30 15:30:16 +0100

    added initialisation of bar graph data

--- lib/bar graph/bar_graph.dart
+++ lib/bar graph/bar_graph.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:fourth_app/bar%20graph/bar_data.dart';
 
 class MyBarGraph extends StatelessWidget {
   final double? maxY;
@@ -25,10 +26,33 @@ class MyBarGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // initialise the bar data
+    BarData myBarData = BarData(
+      sunAmount: sunAmount, 
+      monAmount: monAmount, 
+      tueAmount: tueAmount, 
+      wedAmount: wedAmount, 
+      thurAmount: thurAmount, 
+      friAmount: friAmount, 
+      satAmount: satAmount
+    );
+    myBarData.initialiseBarData();
+
     return BarChart(
       BarChartData(
-        maxY: 100,
+        maxY: maxY,
         minY: 0,
+        barGroups: myBarData.barData.map(
+          (data) => BarChartGroupData(
+            x: data.x,
+            barRods: [
+              BarChartRodData(
+                toY: data.y,
+              )
+            ],
+          )
+        ).toList(),
       )
     );
   }
--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:fourth_app/bar%20graph/bar_graph.dart';
+import 'package:provider/provider.dart';
+
+import '../data/expense_data.dart';
 
 class ExpenseSummary extends StatelessWidget {
-  const ExpenseSummary({super.key});
+  final DateTime startOfWeek;
+  const ExpenseSummary({
+    super.key,
+    required this.startOfWeek,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<ExpenseData>(
+      builder: (context, value, child) => SizedBox(
+        height: 200,
+        child: MyBarGraph(
+          maxY: 100,
+          sunAmount: 39,
+          monAmount: 3,
+          tueAmount: 25,
+          wedAmount: 46,
+          thurAmount: 50,
+          friAmount: 73,
+          satAmount: 95,
+        )
+      )
+    );
   }
 }
